c179263875a7ca2805ab4a8fa089f4a1f595b303
Ben Parisot <me@benparisot.com>
2019-06-11 10:57:00 -0700

    write and pass test for getNoteById route

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -28,4 +28,22 @@ describe('note routes', () => {
         });
       });
   });
+
+  it('finds and returns a note by id', () => {
+    return request(app)
+      .post('/api/v1/notes')
+      .send({ title: 'Cute note bro', body: 'Yeah I like this note a lot' })
+      .then(note => {
+        return request(app)
+          .get(`/api/v1/notes/${note.body._id}`)
+          .then(returnedNote => {
+            expect(returnedNote.body).toEqual({
+              _id: note.body._id,
+              title: 'Cute note bro',
+              body: 'Yeah I like this note a lot',
+              __v: 0
+            });
+          });
+      });
+  });
 });
